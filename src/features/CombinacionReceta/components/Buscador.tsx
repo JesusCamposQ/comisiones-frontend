5d7605ba7779a8c36337b60d5d4c103eaca462cd
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-05-06 18:10:26 -0400

    cambios

--- src/features/CombinacionReceta/components/Buscador.tsx
+++ src/features/CombinacionReceta/components/Buscador.tsx
@@ -1,47 +1,65 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import { filtroCombinacionRecetaI } from '../interfaces/comisiones.interface';
 
 export const Buscador = ({
-    setFiltro,
+  filtro,
+  setFiltro,
 }: {
-    setFiltro: (data: filtroCombinacionRecetaI) => void;
+  filtro: filtroCombinacionRecetaI;
+  setFiltro: (data: filtroCombinacionRecetaI) => void;
 }) => {
-    const { register, getValues, setValue } = useForm<filtroCombinacionRecetaI>({
-        defaultValues: {
-            tipoLente: '',
-            material: '',
-            tratamiento: '',
-            marcaLente: '',
-            tipoColorLente: '',
-            rango: '',
-            colorLente: '',
-        },
-    });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFiltro({ ...filtro, [name]: value });
+  };
 
-    const handleInputChange = (field: keyof filtroCombinacionRecetaI) => (
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        setValue(field, event.target.value); 
-        const nuevosValores = {
-            ...getValues(),
-            [field]: event.target.value,
-        };
-        setFiltro(nuevosValores); 
-    };
-
-    return (
-        <div>
-            <h2>Buscador de Lentes</h2>
-            <form>
-                <input placeholder="tipo Lente" {...register('tipoLente')} onChange={handleInputChange('tipoLente')} />
-                <input placeholder="Material" {...register('material')} onChange={handleInputChange('material')} />
-                <input placeholder="Tratamiento" {...register('tratamiento')} onChange={handleInputChange('tratamiento')} />
-                <input placeholder="Marca" {...register('marcaLente')} onChange={handleInputChange('marcaLente')} />
-                <input placeholder="Tipo Color Lente" {...register('tipoColorLente')} onChange={handleInputChange('tipoColorLente')} />
-                <input placeholder="Rangos" {...register('rango')} onChange={handleInputChange('rango')} />
-                <input placeholder="Color" {...register('colorLente')} onChange={handleInputChange('colorLente')} />
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Buscador de Lentes</h2>
+      <form>
+        <input
+          name="tipoLente"
+          placeholder="tipo Lente"
+          value={filtro.tipoLente}
+          onChange={handleChange}
+        />
+        <input
+          name="material"
+          placeholder="Material"
+          value={filtro.material}
+          onChange={handleChange}
+        />
+        <input
+          name="tratamiento"
+          placeholder="Tratamiento"
+          value={filtro.tratamiento}
+          onChange={handleChange}
+        />
+        <input
+          name="marcaLente"
+          placeholder="Marca"
+          value={filtro.marcaLente}
+          onChange={handleChange}
+        />
+        <input
+          name="tipoColorLente"
+          placeholder="Tipo Color Lente"
+          value={filtro.tipoColorLente}
+          onChange={handleChange}
+        />
+        <input
+          name="rango"
+          placeholder="Rangos"
+          value={filtro.rango}
+          onChange={handleChange}
+        />
+        <input
+          name="colorLente"
+          placeholder="Color"
+          value={filtro.colorLente}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
 };
